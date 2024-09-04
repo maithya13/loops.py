f78c9df105b745dbb8de47c0a15138a090b711a7
maithya13 <125432887+maithya13@users.noreply.github.com>
2024-09-04 11:36:51 +0300

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,10 @@
+# for loop example
+for i in range(5):
+    print('This is loop iteration',i)
+
+# while loop example
+countdown=10
+while countdown>0:
+    print('countdown:',countdown)
+    countdown-=1
+print('blastoff!')
